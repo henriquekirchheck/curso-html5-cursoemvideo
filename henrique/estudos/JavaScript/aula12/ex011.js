71f532fead715a7959f71f354d615657a676ca22
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2021-08-10 18:41:44 -0300

    Made ex011 on aula12 in the JavaScript folder

---
+++ henrique/estudos/JavaScript/aula12/ex011.js
@@ -0,0 +1,11 @@
+const idade = 16
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18) {
+    console.log('Voto opcional')
+} else if (idade >= 65){
+    console.log('Voto opcional')
+} else {
+    console.log('Vota')
+}
