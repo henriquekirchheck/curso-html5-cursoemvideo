4e2b4d2f4baefab48a349589df7bb43381b01c26
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2021-08-10 17:32:44 -0300

    Made ex009 in aula11 on the JavaScript folder

---
+++ henrique/estudos/JavaScript/aula11/ex009.js
@@ -0,0 +1,7 @@
+const pais = 'Brazil'
+console.log(`Vivendo no ${pais}`)
+if (pais === 'Brasil' || pais === 'Brazil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é estranjeiro')
+}
