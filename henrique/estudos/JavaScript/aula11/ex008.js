62e80e96006ebe4329fcf0932506bdf5b36d6974
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2021-08-10 17:24:24 -0300

    Made ex008 in aula11 on the JavaScript folder

---
+++ henrique/estudos/JavaScript/aula11/ex008.js
@@ -0,0 +1,6 @@
+const vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
