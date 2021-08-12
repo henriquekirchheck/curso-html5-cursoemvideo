b5aafa8133fc2637b503caadbb80683d889add7b
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2021-08-12 14:30:25 -0300

    Updated ds005 in desafios on the JavaScript folder

--- henrique/estudos/JavaScript/desafios/ds004/script.js
+++ henrique/estudos/JavaScript/desafios/ds004/script.js
@@ -1,16 +1,16 @@
 function createTimesTable() {
     const numb = Number(document.querySelector('input#num').value)
     const selTT = document.querySelector('select#selTimesTable')
-    let result
     selTT.setAttribute('style', 'block')
-    selTT.innerHTML = ''
     if (numb === NaN) {
         alert('Por favor digite um numero!')
     } else {
+        selTT.innerHTML = null
         for(let x = 1; x <= 10; x++) {
-            result = numb * x
-            console.log(`${numb} x ${x} = ${result}`)
-            selTT.innerHTML += `<option>${numb} x ${x} = ${result}</option>`
+            let option = document.createElement('option')
+            option.text = `${numb} x ${x} = ${numb*x}`
+            option.value = `tab${x}`
+            selTT.appendChild(option)
         }
     }
 }
